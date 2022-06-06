a141adbd6fa7b4674917300b68c5c48798a00c27
kandothshahnas <kandothshahnas@icloud.com>
2022-06-06 16:47:18 -0700

    updated utility

--- utilities/xlUtiliy.py
+++ utilities/xlUtiliy.py
@@ -9,24 +9,21 @@ def get_row_count(file, sheetname):
 
 
 def get_column_count(file, sheetname):
-    #workbook = openpyxl.load_workbook(file)
-    #sheet = workbook.get_sheet_by_name(sheetname)
+    
     workbook = load_workbook(file)
     sheet = workbook[sheetname]
     return sheet.max_column
 
 
 def read_data(file, sheetname, rownum, columnno):
-    #workbook = openpyxl.load_workbook(file)
-    #sheet = workbook.get_sheet_by_name(sheetname)
+   
     workbook = load_workbook(file)
     sheet = workbook[sheetname]
     return sheet.cell(row=rownum, column=columnno).value
 
 
 def write_data(file, sheetname, rownum, columnno, data):
-    #workbook = openpyxl.load_workbook(file)
-    #sheet = workbook.get_sheet_by_name(sheetname)
+    
     workbook = load_workbook(file)
     sheet = workbook[sheetname]
     sheet.cell(row=rownum, column=columnno).value = data
